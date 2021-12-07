e671b0621f3c90e6019487df0bc60bb189956260
nkress <nicholas.kress@softlinkint.com>
2021-12-07 20:06:21 +1000

    Day 6 - Part 2

--- src/days/six/DaySix.java
+++ src/days/six/DaySix.java
@@ -14,36 +14,33 @@ public class DaySix {
   private static final int DAY = 6;
   private static final String INPUT_FILE = "data/" + DAY + "/input.txt";
 
-  private static final int SIMULATION_DAYS = 80;
+  private static final int SIMULATION_DAYS = 256;
 
   public static void main(String args[]) {
     String input = Util.getFileAsList(INPUT_FILE).get(0);
-    List<LanternFish> intialSchool = Arrays.stream(input.split(","))
+    List<Integer> inputSpawns = Arrays.stream(input.split(","))
                                      .map(Integer::parseInt)
-                                     .map(LanternFish::new)
                                      .collect(Collectors.toList());
 
-    List<LanternFish> finalSchool = process(intialSchool, 1);
+    long[] fish = new long[] {0, 0, 0, 0, 0, 0, 0, 0, 0};
 
+    inputSpawns.forEach(e -> fish[e]++);
 
-    int numFishInSchool = finalSchool.size(); // account for marker-fish
-    Util.processResult(Integer.toString(numFishInSchool), DAY, 0);
-
-  }
-
-  static List<LanternFish> process(List<LanternFish> school, int day) {
-    List<LanternFish> nextSchool = new ArrayList<>();
-    for (LanternFish fish: school) {
-      LanternFish babyFish = fish.step();
-      if (babyFish != null) {
-        nextSchool.add(babyFish);
+    int day = 0;
+    while (day < SIMULATION_DAYS) {
+      long temp = fish[0];
+      for (int i = 0; i < fish.length - 1; i++) {
+        fish[i] = fish[i + 1];
       }
-      nextSchool.add(fish);
+      fish[6] += temp;
+      fish[8] = temp;
+
+      day++;
     }
-    if (day < SIMULATION_DAYS) {
-      return process(nextSchool, ++day);
-    } else {
-      return nextSchool;
-    }
+
+    long total = Arrays.stream(fish).sum();
+
+    Util.processResult(Long.toString(total), DAY, 2);
+
   }
 }
