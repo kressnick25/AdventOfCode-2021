79c8ebad1800bebce5dc4aafec464d1360d0afc4
nkress <nicholas.kress@softlinkint.com>
2021-12-07 20:42:34 +1000

    Day 7 - Part 2

--- src/days/seven/DaySeven.java
+++ src/days/seven/DaySeven.java
@@ -3,12 +3,16 @@ package days.seven;
 import util.Util;
 
 import java.util.Arrays;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 import java.util.stream.Collectors;
 
 public class DaySeven {
   private static final int DAY = 7;
   private static final String INPUT_FILE = "data/" + DAY + "/input.txt";
+
+  private static final Map<Long, Long> costLookup = new HashMap<>();
 
   public static void main(String args[]) {
     List<Integer> positions = Arrays.stream(Util.getFileAsList(INPUT_FILE).get(0).split(","))
@@ -21,13 +25,25 @@ public class DaySeven {
     for (int i = 0; i <= max; i++) {
       currentSum = 0;
       for (int position : positions) {
-        currentSum += Math.abs(position - i);
+        currentSum += getFuelCost(Math.abs(position - i));
       }
       if (currentSum < minSum) {
         minSum = currentSum;
       }
     }
 
-    Util.processResult(Long.toString(minSum), DAY, 1);
+    Util.processResult(Long.toString(minSum), DAY, 2);
+  }
+
+  static long getFuelCost(long n) {
+    // we can reuse these values, cache them
+    if (costLookup.containsKey(n)) return costLookup.get(n);
+
+    long total = 0;
+    for (int i = 0; i < n; i++) {
+      total += i + 1;
+    }
+    costLookup.put(n, total);
+    return total;
   }
 }
